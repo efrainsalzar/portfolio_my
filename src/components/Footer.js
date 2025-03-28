6cce3c192135e71e26f45a43b3fa7680fb187889
Muss <efrainsalzarsantos@gmail.com>
2025-03-28 19:37:54 -0400

    footer con iconos de redes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,49 +1,29 @@
 import React from "react";
 import "./Footer.css";
 
+import logoX from "../assets/icons/x-social-media-logo-icon.svg";
+import logoLinkedin from "../assets/icons/linkedin-app-icon.svg";
+
 function Footer() {
 
-  const socialLink = [
-    {
-      name: 'GitHub',
-      url: 'https://github.com/miusuario',
-      icon: 'fa-github',
-      className: 'github'
-    },
-    {
-      name: 'LinkedIn',
-      url: 'https://www.linkedin.com/in/miusuario',
-      icon: 'fa-linkedin',
-      className: 'linkedin'
-    },
-    {
-      name: 'X',
-      url: 'https://x.com/',
-      icon: 'fa-times',
-      className: 'x'
-    }
-  ];
-
+  
   return (
     <footer className="footer">
       <div className="footer-container">
-        <p>inspirado en:&nbsp;
+        <p>Inspirado en:&nbsp;
           <a href="https://cristianorrego.dev" target="_blank" rel="noopener noreferrer">
             cristianorrego.dev
           </a>
         </p>
         <div className="social-links">
-          {socialLink.map((link, index) => (
-            <a
-              key={index}
-              href={link.url}
-              target="_blank"
-              rel="noopener noreferrer"
-              className={`social-logo ${link.className}`}
-            >
-              <i className={`fa ${link.icon}`}></i>
-            </a>
-          ))}
+          {/* Icono de X */}
+          <a href="https://x.com/tu_usuario" target="_blank" rel="noopener noreferrer">
+            <img src={logoX} alt="X" className="social-icon" />
+          </a>
+          {/* Icono de LinkedIn */}
+          <a href="https://www.linkedin.com/in/tu_usuario" target="_blank" rel="noopener noreferrer">
+            <img src={logoLinkedin} alt="LinkedIn" className="social-icon" />
+          </a>
         </div>
       </div>
     </footer>
